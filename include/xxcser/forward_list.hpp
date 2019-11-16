c31dfa97fc9a8e161a8f48b1a78266e726d521fd
Cxx <37257545+Cxx-mlr@users.noreply.github.com>
2019-11-16 08:39:01 -0500

    unimplemented

--- include/xxcser/forward_list.hpp
+++ include/xxcser/forward_list.hpp
@@ -6,11 +6,11 @@
 
 namespace io {
 	template <class t>
-	auto write(const std::forward_list <t>& forward_list) -> decltype(io::write(forward_list.front())) {
+	auto write(const std::forward_list <t>& forward_list) -> decltype(io::write(*forward_list.begin())) {
 	}
 
 	template <class t>
-	auto read(std::forward_list <t>& forward_list) -> decltype(io::read(forward_list.front())) {
+	auto read(std::forward_list <t>& forward_list) -> decltype(io::read(*forward_list.begin())) {
 	}
 }
 
